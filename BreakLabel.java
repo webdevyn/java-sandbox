255e437be60e80f0d8429efba5456f6c46321fdf
webdevyn <sendtodevyn@gmail.com>
2022-05-21 11:29:00 -0700

    working on break and break with label

---
+++ BreakLabel.java
@@ -0,0 +1,25 @@
+public class BreakLabel {
+    public static void main (String[] args) {
+        int i;
+
+        for(i=1; i<4; i++) {
+        one: {
+        two:   {
+        three:   {
+            System.out.println("\ni is " + i);
+            if(i==1) break one; //break to label
+            if(i==2) break two;
+            if(i==3) break three;
+
+            //this is never reached
+            System.out.println("never prints");
+        }
+            System.out.println("After block three.");
+        }
+            System.out.println("After block two.");
+        }
+            System.out.println("After block one.");
+        }
+        System.out.println("After for.");
+    }
+}
