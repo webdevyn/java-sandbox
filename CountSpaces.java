2d68af4d2772092227d9857c92f800f33b11ec03
webdevyn <sendtodevyn@gmail.com>
2022-05-30 09:29:58 -0700

    add README

---
+++ CountSpaces.java
@@ -0,0 +1,17 @@
+public class CountSpaces {
+    public static void main(String[] args)
+    throws java.io.IOException {
+        char ch;
+        int spaces = 0;
+
+        System.out.println("Enter a period to stop.");
+
+        do {
+            ch = (char) System.in.read();
+            if(ch == ' ') spaces++;
+        } while(ch != '.');
+
+        System.out.println("Spaces: " + spaces);
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#Overview
+
+This is a sandbox for java exercises and practice.
